54d28598d6a83f45bfd308a3b38eeebf394525d3
Tobuchas <tom@buchtovi.cz>
2025-12-02 13:49:09 +0100

    Changed Rotation Angle Limit System

--- GDGame/Scripts/Traps/RotatingTrap.cs
+++ GDGame/Scripts/Traps/RotatingTrap.cs
@@ -12,12 +12,16 @@ namespace GDGame.Scripts.Traps
     {
         #region Fields
         private float _rotSpeed = 5f;
+        private float _startAngle = -90f;
+        private float _endAngle = 90f;
+        private float _currentAngle = 0f;
+        private bool _rotatingClockwise = true;
         #endregion
 
         #region Constructors
         public RotatingTrap(int id, float rotSpeed) : base(id)
         {
-            _trapGO = ModelGenerator.Instance.GenerateCube(new Vector3(-10, 10, 0), Vector3.Zero, new Vector3(5, 5, 5), "ground_grass", AppData.TRAP_NAME + id);
+            _trapGO = ModelGenerator.Instance.GenerateCube(new Vector3(0, 10, -10), Vector3.Zero, new Vector3(5, 5, 5), "ground_grass", AppData.TRAP_NAME + id);
             _trapGO.AddComponent<BoxCollider>();
             SceneController.AddToCurrentScene(_trapGO);
             _rotSpeed = rotSpeed;
@@ -27,11 +31,28 @@ namespace GDGame.Scripts.Traps
         #region Methods
         public override void UpdateTrap()
         {
-            TrapGO.Transform.RotateBy(Quaternion.CreateFromAxisAngle(Vector3.Up, MathHelper.ToRadians(_rotSpeed)));
-            if (Quaternion.Dot(TrapGO.Transform.Rotation, Quaternion.CreateFromAxisAngle(Vector3.Up, MathHelper.Pi)) > 0.9f)
+            TrapGO.Transform.RotateBy(Quaternion.CreateFromAxisAngle(Vector3.Forward, MathHelper.ToRadians(_rotSpeed)));
+            if (_rotatingClockwise)
             {
+                _currentAngle++;
+            }
+            else
+            {
+                _currentAngle--;
+            }
+
+            if (_currentAngle >= _endAngle)
+            {
+                _rotatingClockwise = false;
                 flip();
             }
+            if (_currentAngle <= _startAngle)
+            {
+                _rotatingClockwise = true;
+                flip();
+            }
+
+
         }
 
         public override void InitTrap()
